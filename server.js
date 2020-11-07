958ad0352cdc01ba939b2cd221986ca006b275a7
Sarafa2n <slaft3@yandex.ru>
2020-11-07 14:05:39 +0300

    CP-70: Переделать веб-сервер на https

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
+
+httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/musicexpress.sarafa2n.ru/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/musicexpress.sarafa2n.ru/fullchain.pem'),
+};
 
 app.all('*', (req, res) => {
     res.sendFile(`${__dirname}/dist/index.html`);
 });
 
-app.listen(port);
+https.createServer(httpsOptions, app).listen(443);
